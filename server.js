12e7c4d649ddc43109f8e1ae358918575da45a1e
Flor Lafuente <lafuente.florencia@gmail.com>
2020-03-23 13:09:48 -0300

    Fetching poems :)

--- server.js
+++ server.js
@@ -1,10 +1,51 @@
 const express = require('express');
 const app = express()
+const Twit = require('twit')
+const axios = require('axios')
+
+require('dotenv').config()
+
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello World!')
 })
 
-app.listen(3000, () => {
+app.get('/tweet', async (req, res) => {
+  try {
+    const makeTweet = await getQuote()
+  } catch (err) {
+    console.log(err)
+    res.send("Something didn't work out :(")
+  }
+})
+
+// Twitter app config
+const T = new Twit({
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET_KEY,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+})
+
+// Fetch quote from API
+const getQuote = async() => {
+  try {
+    const maxPoems = process.env.MAX_NUMBER
+    console.log(maxPoems)
+    const number = Math.floor(Math.random() * maxPoems) + 1
+    console.log(number)
+    const apiUrl = `${process.env.API_URL}/poemas/${number}`
+
+    const res = await axios.get(apiUrl)
+    const poem = res.data
+    console.log(poem)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+
+app.listen(port, () => {
   console.log('App started!')
 })
